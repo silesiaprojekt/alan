17701117c72d882df5607e9075f0cc5900ae1a55
Silesiaprojekt <biuro@silesiaprojekt.pl>
2016-10-26 19:58:44 +0200

    New concept and fontawesome

--- js/main.js
+++ js/main.js
@@ -5,32 +5,52 @@ myApp.controller('GreetingController', ['$scope', function($scope) {
 $scope.team = [{
   "id": 1,
   "project": "Cream",
-  "leader": "Andrzej",
+  "leader": "Andrzej Kowalski",
   "email": "andrzej@poczta.pl",
-  "subleader": "Norbert",
+  "subleader": "Norbert Nowakowski",
   "value": "40%",
   "employees": [
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
     {"firstName":"Marek", "lastName":"Nowak", "interest":10}
 ]
 }, {
   "id": 2,
-  "project": "Projekt 2",
-  "leader": "Gilbert2",
-  "subleader": "Norbert2",
+  "project": "brackets",
+  "leader": "Andrzej Kowalski",
+  "subleader": "Andrzej Kowalski",
   "value": "20%",
   "employees": [
     {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
+      {"firstName":"John", "lastName":"Doe", "interest":20},
     {"firstName":"Anna", "lastName":"Smith", "interest":80}
 ]
 }, {
   "id": 3,
-  "project": "Projekt 2",
-  "leader": "Gilbert3",
-  "subleader": "Norbert3",
+  "project": "mobile zone",
+  "leader": "Marek Nowakowski",
+  "subleader": "Marek Nowakowski",
   "value": "80%",
   "employees": [
     {"firstName":"John", "lastName":"Doe", "interest":40},
     {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
+      {"firstName":"Anna", "lastName":"Smith", "interest":50},
     {"firstName":"Peter", "lastName":"Jones", "interest":20}
 ]
 }]
