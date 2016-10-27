5822bc47279014d39e9d4d9d4049a8900981ce25
Kamil Grygiel <kamil.grygiel@alan-systems.com>
2016-10-27 15:02:52 +0200

    New

--- js/main.js
+++ js/main.js
@@ -10,42 +10,41 @@ $scope.team = [{
   "subleader": "Norbert Nowakowski",
   "value": "40%",
   "employees": [
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male1.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male3.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female3.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male1.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female2.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male1.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male3.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female3.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male1.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female2.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female1.png"}
+      {"firstName":"Marek", "lastName":"", "position":"developer", "img": "male1.png"},
+      {"firstName":"Tomek", "lastName":"", "position":"admin", "img": "male3.png"},
+      {"firstName":"Zenek", "lastName":"", "position":"developer", "img": "female3.png"},
+      {"firstName":"Robert", "lastName":"", "position":"marketing", "img": "male1.png"},
+      {"firstName":"Franek", "lastName":"", "position":"developer", "img": "female2.png"},
+      {"firstName":"Emil", "lastName":"", "position":"developer", "img": "male1.png"},
+      {"firstName":"Kamil", "lastName":"", "position":"back-end", "img": "male3.png"},
+      {"firstName":"Tomasz", "lastName":"", "position":"developer", "img": "female3.png"},
+      {"firstName":"Rafał", "lastName":"", "position":"developer", "img": "male1.png"},
+      {"firstName":"Zenon", "lastName":"", "position":"back-end", "img": "female2.png"},
+      {"firstName":"Kamil", "lastName":"", "position":"developer", "img": "female1.png"}
 ]
 }, {
   "id": 2,
   "project": "brackets",
-  "leader": "Andrzej Kowalski",
+  "leader": "Marek Nowakowski",
   "subleader": "Andrzej Kowalski",
   "value": "20%",
   "employees": [
-    {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female3.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female2.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male1.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female1.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male3.png"}
+      {"firstName":"Kamil", "lastName":"", "position":"developer", "img": "male3.png"},
+      {"firstName":"Tomasz", "lastName":"", "position":"developer", "img": "female3.png"},
+      {"firstName":"Rafał", "lastName":"", "position":"back-end", "img": "male1.png"},
+      {"firstName":"Zenon", "lastName":"", "position":"ux-designer", "img": "female2.png"}
 ]
 }, {
   "id": 3,
   "project": "mobile zone",
-  "leader": "Marek Nowakowski",
+  "leader": "Ryszard Zawadzki",
   "subleader": "Marek Nowakowski",
   "value": "80%",
   "employees": [
-    {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "male1.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female2.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female3.png"},
-      {"firstName":"John", "lastName":"Doe", "position":"developer", "img": "female1.png"}
+    {"firstName":"Robert", "lastName":"", "position":"developer", "img": "male1.png"},
+      {"firstName":"Franek", "lastName":"", "position":"developer", "img": "female2.png"},
+      {"firstName":"Emil", "lastName":"", "position":"front-end", "img": "male1.png"},
+      {"firstName":"Kamil", "lastName":"", "position":"developer", "img": "male3.png"}
 ]
 }]
 
